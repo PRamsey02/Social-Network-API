e64324eb3a7687e067282fd9b32e71fd3e3c3537
PRamsey02 <prestonkramsey02@icloud.com>
2022-11-28 22:48:36 -0600

    Added reactions, thoughts schema

---
+++ models/Thoughts.js
@@ -0,0 +1,68 @@
+// Requiring Mongoose and Moment
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+// Reactions Schema
+const ReactionsSchema = new Schema ({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        // Moment date format
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    toJson: {
+        getters: true
+    }
+});
+
+// Thoughts Schema
+const ThoughtsSchema = new Schema ({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        // Moment date format
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionsSchema]
+    },
+    {
+    toJson: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+// Require reaction count
+ThoughtsSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+// Creating Thoughts model through Thoughts Schema
+const Thoughts = model('Thoughts', ThoughtsSchema);
+
+// Exporting Thoughts module
+module.exports = Thoughts;

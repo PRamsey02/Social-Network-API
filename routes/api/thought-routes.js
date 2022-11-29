afc0d78f95cc5bd6d8e9ac613bcf83e35dc7ad07
PRamsey02 <prestonkramsey02@icloud.com>
2022-11-28 23:46:59 -0600

    Added Thoughts controller requirements within thought-routes.js

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,31 @@
+// Requiring express route
+const router = require('express').Router();
+
+// Thoughts controller requirements
+const {
+    getAllThoughts,
+    getThoughtsById,
+    createThoughts,
+    updateThoughts,
+    deleteThoughts,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughts-controller');
+
+// GET: /api/thoughts
+router.route('/').get(getAllThoughts);
+
+// GET, PUT, DELETE: /api/thoughts/:id
+router.route('/:id').get(getThoughtsById).put(updateThoughts).delete(deleteThoughts);
+
+// POST: /api/thoughts/:userId
+router.route('/:userId').post(createThoughts);
+
+// POST: /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// DELETE: /api/thoughts/:thoughtId/reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+// Exporting router module
+module.exports = router;
